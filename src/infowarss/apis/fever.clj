e97ec8b912e8bec73736dee2a9159307551e8ddb
Marcel Lauhoff <ml@irq0.org>
2017-05-14 22:57:19 +0200

    Fever API: Improved request logging

--- src/infowarss/apis/fever.clj
+++ src/infowarss/apis/fever.clj
@@ -451,22 +451,27 @@
   (let [op (extract-op req)
         params (:params req)]
     (s/with-fn-validation
-      (log/debugf "[Fever API] OP: %s params: %s" op params)
-      (condp = op
-        :groups (groups)
-        :feeds (feeds)
-        :items (items :since
-                       (swallow-exceptions (Long/parseLong (get params :since_id)))
-                       :max
-                       (swallow-exceptions (Long/parseLong (get params :max_id)))
-                       :with
-                       (swallow-exceptions (map #(Long/parseLong %) (re-seq #"\d+" (get params :with_ids)))))
-        :links (links)
-        :favicons (favicons)
-        :unread_item_ids (unread-item-ids)
-        :saved_item_ids (saved-item-ids)
-        nil (when (contains? (:params req) :id) (handle-write-op req))
-        {}))))
+      (log/debugf "[Fever API] OP: %s params: %s query-string: %s"
+        op params (:query-string req))
+      (let [response (condp = op
+                       :groups (groups)
+                       :feeds (feeds)
+                       :items (items :since
+                                (swallow-exceptions (Long/parseLong (get params :since_id)))
+                                :max
+                                (swallow-exceptions (Long/parseLong (get params :max_id)))
+                                :with
+                                (swallow-exceptions (map #(Long/parseLong %) (re-seq #"\d+" (get params :with_ids)))))
+                       :links (links)
+                       :favicons (favicons)
+                       :unread_item_ids (unread-item-ids)
+                       :saved_item_ids (saved-item-ids)
+                       nil (when (contains? (:params req) :id) (handle-write-op req))
+                       {})]
+        (log/tracef "[Fever API] OP: %s response: %s"
+          op (into {} (map (fn [[k v]] [k (if (number? v) v
+                                              (format "%s: n=%s" (type v) (count v)))]) response)))
+        response))))
 
 
 (defn fever-api
