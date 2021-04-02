8991918efacd60f696aea18afac92aa5f5a05e2e
Marcel Lauhoff <ml@irq0.org>
2021-04-02 11:03:33 +0200

    Refactor db layer: Search

--- src/infowarss/db/search.clj
+++ src/infowarss/db/search.clj
@@ -37,56 +37,20 @@
 (defn refresh-idf []
   (j/execute! db ["refresh materialized view idf_top_words"]))
 
-(defn item-tf-idf [id]
-  (j/query db [(str
-                "select term_tf->>0 as term, (term_tf->>1)::float as tf, idf_top_words.ln as idf, (term_tf->>1)::float * idf_top_words.ln as tf_idf "
-                " from "
-                "(select id, jsonb_array_elements(nlp_top->'words') as term_tf from items where id = ?) as i "
-                "inner join "
-                "idf_top_words on (term_tf->0 = idf_top_words.term) "
-                "order by tf_idf asc") id]))
-
-;; TF = term frequence
-;; IDF inverse document frequency
-
-(defn top-idf [max]
-  (j/query db ["select term, ln from idf_top_words limit ?" max]))
-
-(defn recommendations
-  "Find similar items for id"
-  [_])
-  ;; compute cosine distance to all docs in db
-
 (defn saved-items-tf-idf []
-  (rest (j/query db [(str "select "
-                          "id, "
-                          "json_agg(json_build_array(term_tf->>0 , "
-                          " (term_tf->>1)::float * idf_top_words.ln )) "
-                          " from "
-                          "(select id, jsonb_array_elements(nlp_top->'words') as term_tf "
-                          "from items "
-                          "where exist_inline(tags, 'saved') or (items.type = 'bookmark' and exist_inline(tags, 'unread')) ) as i "
-                          "inner join "
-                          "idf_top_words on (term_tf->0 = idf_top_words.term) "
-                          "group by id ")]
-                 {:as-arrays? true
-                  :row-fn (fn [[id term-tf-idf]]
-                            (assoc (into {} term-tf-idf) "item_id" (double id)))})))
-
-;; make adjustmens here to limit the number of words to use in k means
+  (rest (sql/saved-items-tf-idf
+   db
+   nil
+   {}
+   {:as-arrays? true
+    :row-fn (fn [[id term-tf-idf]]
+              (assoc (into {} term-tf-idf) "item_id" (double id)))})))
 
 
 (defn saved-items-tf-idf-terms []
-  (first (second (j/query db [(str "select array_agg(foo.term) from (select distinct "
-                                   "term_tf->>0 as term, "
-                                   "(term_tf->>1)::float * idf_top_words.ln as tf_idf "
-                                   " from "
-                                   "(select id, jsonb_array_elements(nlp_top->'words') as term_tf "
-                                   "from items "
-                                   "where exist_inline(tags, 'saved') or (items.type = 'bookmark') and exist_inline(tags, 'unread')) as i "
-                                   "inner join "
-                                   "idf_top_words on (term_tf->0 = idf_top_words.term) "
-                                   "where (term_tf->>1)::float > 1 and length(term_tf->>0) > 4 and not (term_tf->>0) like '%/%'"
-                                   ") as foo")]
-                          {:as-arrays? true}))))
+  (first (second (sql/saved-items-tf-idf-terms
+                  db
+                  nil
+                  {}
+                  {:as-arrays? true}))))
 
--- src/infowarss/db/sql/search.sql
+++ src/infowarss/db/sql/search.sql
@@ -10,3 +10,35 @@ where document @@ to_tsquery('english', :query)
 --~ (when (:time-ago params) "and ts > :time-ago")
 --~ (when (:source-key params) "and key = :source-key")
 order by rank desc
+
+
+-- :name saved-items-tf-idf :? :raw
+select
+  id,
+  json_agg(json_build_array(term_tf->>0 , (term_tf->>1)::float * idf_top_words.ln ))
+from
+  ( select id, jsonb_array_elements(nlp_top->'words') as term_tf
+    from items
+    where exist_inline(tags, 'saved')
+      or (items.type = 'bookmark' and exist_inline(tags, 'unread'))
+  ) as i
+inner join
+  idf_top_words on (term_tf->0 = idf_top_words.term)
+group by id
+
+
+-- :name saved-items-tf-idf-terms :? :raw
+select array_agg(foo.term)
+from (
+  select distinct term_tf->>0 as term, (term_tf->>1)::float * idf_top_words.ln as tf_idf
+  from (
+    select id, jsonb_array_elements(nlp_top->'words') as term_tf
+    from items
+    where exist_inline(tags, 'saved') or (items.type = 'bookmark')
+      and exist_inline(tags, 'unread')) as i
+  inner join idf_top_words on (term_tf->0 = idf_top_words.term)
+  where
+    (term_tf->>1)::float > 1
+    and length(term_tf->>0) > 4
+    and not (term_tf->>0) like '%/%'
+) as foo
